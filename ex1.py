2fabab53a27968111381202cff6b6cfb93159948
TIMUR <1tkurbanov@gmail.com>
2019-05-13 17:00:40 -0400

    1 ex

---
+++ ex1.py
@@ -0,0 +1,24 @@
+class Person:
+  def __init__(self, emotions):
+    self.pizza = emotions
+
+  def __str__(self):
+    return f"Emotions: {self.pizza}"
+
+  def pasta(self):
+    for emotion, level in self.pizza.items():
+      if level == 1:
+        print(f"You look {emotion}")
+      elif level == 2:
+        print(f"You look {emotion}")
+      elif level == 3:
+        print(f"You look {emotion}")
+      
+emotions = {
+  'tired': 1,
+  'OK': 2,
+  'good': 3
+}
+person1 = Person(emotions)
+
+person1.pasta()
